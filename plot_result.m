d283fcce16d6faf11eaa21db231bce4433cb5b68
YZ6565785 <6565785@qq.com>
2021-03-03 23:03:28 +0000

    added some plots

--- plot_result.m
+++ plot_result.m
@@ -1,17 +1,42 @@
 %
 % run cw1 before run this!!!
 
+
+%% plot after multi-epoch positioning
+close all;
+x = Solutions_NED(:,1); % lat
+y = Solutions_NED(:,2); % long
+plot(x,y)
+savefig("multi_epoch_pos");
+
+%% plot after Kalman Filter with all measurement 
+close all;
+x = States_out_NED(:,1); % lat
+y = States_out_NED(:,2); % long
+plot(x,y)
+savefig("Kalman_Filter_pos");
+
+
+%% plot after Kalman Filter and outlier measurement removed
+close all;
+x = States_NED(:,1); % lat
+y = States_NED(:,2); % long
+plot(x,y)
+savefig("Kalman_Filter_pos");
+
+%% final position | after DR GNSS integration
 close all;
 num = [1:size(correct_Deg,1)];
-
 % plot corrected position
-x = correct_Deg(:,2);
-y = correct_Deg(:,3);
-% y = [1:size(Solutions_NED,1)];
+x = correct_Deg(:,2); % lat
+y = correct_Deg(:,3); % long
+
 plot(x,y)
 savefig("corrected_pos");
 
-
+%% final velocity | after DR GNSS integration
+close all;
+num = [1:size(correct_Deg,1)];
 % plot corrected velocity
 v_north = correct_Deg(:,4);
 v_east = correct_Deg(:,5);
@@ -20,5 +45,4 @@ plot(num,v_north);
 savefig("corrected_vel_north");
 plot(num,v_east);
 savefig("corrected_vel_east");
-% plot(Solutions_NED(:,1), Solutions_NED(:,2))
 
