bc13d73bee4c51b1b9dd33530001488f166e1f73
naiyuan1997 <79099411+naiyuan1997@users.noreply.github.com>
2021-03-04 01:51:06 +0000

    Update cw1_smooth_heading.m

--- cw1_smooth_heading.m
+++ cw1_smooth_heading.m
@@ -1,13 +1,17 @@
 function [Heading] = cw1_smooth_heading(D)
 Define_Constants;
-weight = 0.85;
+%define the compass weighting to the gyro speed%
+weight = 0.75;
 DR = table2array(D);
+%inport the heading measurements data and gyroscope angular speed%
 H = DR(:,7)*deg_to_rad;
 R = DR(:,6);
+%pick initial status for heading%
 Heading = [H(1)];
 heading = H(1);
 for i = 2:length(DR)
-    heading = weight*H(i)+(1-weight)*(heading+0.5*(R(i-1)+R(i))*0.5);
+    %Gyro-Smoothed Magnetic Heading%
+    heading = weight*H(i)+(1-weight)*(heading+0.5*(R(i-1)+R(i))*0.5); 
     Heading = [Heading; heading];
 end
 Heading = Heading*rad_to_deg;
